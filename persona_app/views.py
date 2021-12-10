37cafda070b5d2add8810fe78984bb9787525b57
aanan <anania.alexandre@gmail.com>
2021-12-10 11:01:44 +0100

    Add variables to get datas from the randomuserapi

--- persona_app/views.py
+++ persona_app/views.py
@@ -25,7 +25,31 @@ def persona_details(request, id):
 
 def persona_generate(request):
     data = requests.get('https://randomuser.me/api?nat=fr').json()
-    print(data['results'][0]['name']['first'])
+    first_name = data['results'][0]['name']['first']
+    print(first_name)
+    last_name = data['results'][0]['name']['last']
+    print(last_name)
+    address_street = data['results'][0]['location']['street']['name']
+    print(address_street)
+    address_number = data['results'][0]['location']['street']['number']
+    print(address_number)
+    city = data['results'][0]['location']['city']
+    print(city)
+    country = data['results'][0]['location']['country']
+    print(country)
+    postcode = data['results'][0]['location']['postcode']
+    print(postcode)
+    email = data['results'][0]['email']
+    print(email)
+    username = data['results'][0]['login']['username']
+    print(username)
+    password = data['results'][0]['login']['password']
+    print(password)
+    age = data['results'][0]['registered']['age']
+    print(age)
+    picture = data['results'][0]['picture']['medium']
+    print(picture)
+    # Persona.objects.__new__(Persona)
     return HttpResponse(f'Generate persona')
     #return redirect('persona_details' persona.id)
     #return render(request, 'persona_app/persona_details.html', context)
